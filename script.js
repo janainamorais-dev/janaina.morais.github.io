94c2a274f9e6bcbca39a3c4992fc4e19ffddc23d
janazepam <janamorais0703@gmail.com>
2026-02-10 20:44:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
 console.log("%cPortfólio da Janaína carregado com sucesso 🚀", "color: #22c55e; font-size: 16px; font-weight: bold;");
-
-console.log("%c👀 Olá, curioso! Se você está vendo isso, significa que gosta de explorar detalhes técnicos. 😎", "color: #38bdf8; font-size: 14px;");
-
-console.log(`%c
-   _____       _         _       
-  |  __ \\     | |       | |      
-  | |__) |   _| | _____ | |_ ___ 
-  |  ___/ | | | |/ / _ \\| __/ _ \\
-  | |   | |_| |   < (_) | ||  __/
-  |_|    \\__,_|_|\\_\\___/ \\__\\___|
-                               
-Portfólio da Janaína 🚀
-`, "color: #22c55e; font-weight: bold;");
-
-
-console.log("%cSe você gosta do que viu, me encontre no LinkedIn: https://www.linkedin.com/in/janaína-de-morais-santos-532374225", "color: #facc15; font-size: 14px;");
